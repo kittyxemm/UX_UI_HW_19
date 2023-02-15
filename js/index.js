9b281a7fde0d2d8f1886cb789924fb45cb1d10c0
kittyxemm <112814637+kittyxemm@users.noreply.github.com>
2023-02-15 18:53:41 -0500

    Updated image locations

---
+++ js/index.js
@@ -0,0 +1,23 @@
+console.log("Katherine, you did it! Look at you!");
+
+// $(window).load(function() {
+// $('#preloader').fadeOut();
+// })
+
+// setTimeout(fade, 500);
+
+
+
+// function fade() {
+//     $('#preloader').fadeOut('slow')
+// }
+
+// setTimeout(fade, 3000);
+
+
+
+function fade() {
+    $('#preloader').fadeOut('slow')
+}
+
+setTimeout(fade, 3000);
