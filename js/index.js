01d52bd1109afc2c69eeda8bb6c33f1b36a6d78d
kittyxemm <112814637+kittyxemm@users.noreply.github.com>
2023-02-16 14:35:34 -0500

    Updated code per Jonathan's notes to correct jQuery preloader animation

--- js/index.js
+++ js/index.js
@@ -1,22 +1,5 @@
-// console.log("Katherine, you did it! Look at you!");
-
-// $(window).load(function() {
-// $('#preloader').fadeOut();
-// })
-
-// setTimeout(fade, 5000);
-
-
-
 function fade() {
-    $("#preloader").fadeOut('slow')
+    $('#preloader').fadeOut('slow');
 }
 
-setTimeout(fade, 3000);
-
-
-
-// function fade() {
-//     $('#preloader').fadeOut("slow");
-//     }
-//     setTimeout(fade, 3000);
+setTimeout(fade, 6000);
